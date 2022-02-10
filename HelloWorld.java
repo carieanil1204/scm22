c0287e88997b62d7b62d76a4560bc00bdb8fbf86
anil <anil@vit.ac.in>
2022-02-10 06:04:01 +0000

    edited print stmt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,5 @@
 class HelloWorld {
-
-	public int Set(int a)
-	{
-	}
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-	Syetem.out.println("welcome to git");
+        System.out.println("SCM"); 
     }
 }
