459d779a7c36bff657dc6fe0dc6359bb647d991a
anil <anil@vit.ac.in>
2022-02-10 05:55:25 +0000

    my third commit added method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,8 @@
 class HelloWorld {
+
+	public int Set(int a)
+	{
+	}
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 	Syetem.out.println("welcome to git");
